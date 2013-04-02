195def384ce5e070b79aa0b8b604c5a297c517d9
Zhong Zhuang <naituida@foxmail.com>
2013-04-02 13:14:51 +0800

    using psutil

--- proxy_monitor.py
+++ proxy_monitor.py
@@ -2,45 +2,51 @@ import time
 import subprocess
 import os
 import string
+import psutil
+
 
 def cur_time():
     return time.strftime("%Y-%m-%d %H:%M:%S",time.localtime())
 
 # TODO: more accurate criteria 
 def is_stuck(proc):
-    return proc['cpu'] > '98'
+    return proc.get_cpu_percent(interval=0.1)>98
 
-proxy_stub = 'python ~/stratum-mining-proxy/mining_proxy.py -o 192.95.29.175 -gp '
+
+# 192.95.29.175  btcguild
+# us.ozco.in     ozcoin
+def build_cmd(server,port):
+    return 'python ~/stratum-mining-proxy/mining_proxy.py -o '+ server + ' -gp ' + port + ' >/dev/null 2>/dev/null &'
+
+def get_proxy_pids():
+    pids = []
+    for p in psutil.process_iter():
+        if "-gp" in p.cmdline:
+            pids.append(p.pid)
+    return pids
 
 
 while(True):
-    procs = []
-    cmd = 'top -n1 -c -b | grep proxy | grep -v grep | awk \'{print $1,$9,$NF}\''
-    p = subprocess.Popen(cmd, stdout=subprocess.PIPE,shell=True)
-    while True:
-        line = p.stdout.readline()
-        if line != '':
-            p_info = line.split()
-            proc = {'pid':p_info[0],'cpu':p_info[1],'port':p_info[2]}
-            procs.append(proc)
-        else:
-            break    
-
-    p.wait()
-       
+    procs = map(psutil.Process,get_proxy_pids())
     for proc in procs:
         if is_stuck(proc):
+            port = proc.cmdline[-1]
+            server = proc.cmdline[3]
             # restart process
-            print 'port ' + proc['port'] + ' is stuck!'
-            os.kill(string.atoi(proc['pid']),9)
-            print 'killed process '+proc['pid']
-            respawn = proxy_stub + proc['port'] + ' > /dev/null 2> /dev/null &'
+            print 'port ' + port + ' is stuck!'
+            os.kill(proc.pid,9)
+            print 'killed process '+ str(proc.pid)
+            respawn = build_cmd(server,port)
             print respawn
             subprocess.Popen(respawn,shell=True)
-            print 'restart port '+proc['port']
+            print 'restart port '+port
         else:
             continue
             
     print '=================' + cur_time() + '=================='
-    time.sleep(5)
+    time.sleep(10)
    
+
+
+
+

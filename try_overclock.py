3e46cded69a99bdd56463f294a43524dd694f3aa
david <david@mserver2.mserver>
2013-05-01 21:30:16 +0800

    TODO:reset board

---
+++ try_overclock.py
@@ -0,0 +1,78 @@
+import yaml
+from urllib2 import urlopen
+import re
+from time import sleep
+ 
+f=open('config.yaml')
+config=yaml.safe_load(f)
+f.close()
+
+def make_range(s):
+    return eval('range'+s)
+
+def parse(obj):
+    if isinstance(obj,str):
+        return make_range(obj)
+    else:
+        return [obj]
+
+def make_ip(a,b):
+    return 'http://192.168.'+str(a)+'.'+str(b)+':8000'
+
+def get_clock(ip):
+    try:
+        page = urlopen(ip).read()
+        find_clock=re.search("Clock selected: ([a-zA-Z]+)",page)
+        (status,)=find_clock.groups()
+        return status
+    except:
+        return 'fail to connect '+ip
+
+def count_X(ip):
+    try:
+        page = urlopen(ip).read()
+        find_status=re.search("([OX]+)",page)
+        status=find_clock.group()
+        return status.count("X")
+    except:
+        return 'fail to connect '+ip
+    
+def toggle_clock(ip):
+    urlopen(ip+'/Sw_Clock')
+
+def overclock():
+    for k in config.keys():
+        res=map(parse,config[k])
+        domain = [item for sublist in res for item in sublist]
+        ips = map(lambda x:make_ip(k,x),domain)
+        for ip in ips:
+            print ('overclocking node '+ip)
+            status = get_clock(ip)
+            if( status == 'Low'):
+                print 'toggle '+ip+' to High'
+                toggle_clock(ip)
+            else:
+                if(status == 'High'):
+                    high+=1
+                else:
+                    print status
+
+def downclock():
+    for k in config.keys():
+        res=map(parse,config[k])
+        domain = [item for sublist in res for item in sublist]
+        ips = map(lambda x:make_ip(k,x),domain)
+        for ip in ips:
+            print ('checking node '+ip)
+            count = count_X(ip)
+            status = get_clock(ip)
+            if( status == 'High' and count>=4):
+                print('downclock '+ip)
+                toggle_clock(ip)
+                
+
+overclock()
+sleep(60)
+downclock()
+
+

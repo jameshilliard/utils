326ca4d02235ca7726970f34fbd77599e6a1e71d
david <david@mserver2.mserver>
2013-05-02 18:58:41 +0800

    auto config

---
+++ config_auto.py
@@ -0,0 +1,40 @@
+import urllib2, urllib
+import requests
+import sys
+
+rack = int(sys.argv[1])
+board_begin = int(sys.argv[2])
+board_end = int(sys.argv[3])
+port = int(sys.argv[4])
+worker = sys.argv[5]
+
+boards=range(board_begin,board_end+1)
+
+# def make_two_digit (num):
+#     if num >=0 and num < 10:
+#         return '0' + str(num)
+#     else:
+#        return str(num)
+
+for board in boards:
+    data = {
+        'JMIP' : '192.168.' + str(rack) + '.' + str(board),
+        'JMSK' : '255.255.0.0',
+        'JGTW' : '192.168.1.1',
+        'PDNS' : '8.8.8.8',
+        'MURL' : '192.168.0.10,192.168.0.10',
+        'MPRT' : str(port)+','+str(port),
+        'USPA' : 'asicminer_' + worker + ':wasabi,asicminer_' +  worker + ':wasabi',
+        'JGTV' : '0',
+        }
+    print data
+    url     = 'http://192.168.1.254:8000/Upload_Data'
+    while(True):
+        try:
+            r = requests.post(url, data)
+            print r.content
+        except:
+            print "Cannot connect to rack " + str(rack) + ", board " + str(board)
+            continue
+        break
+
